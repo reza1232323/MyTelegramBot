4cecd50c7ee10833f8aaea050191c86f223d98ff
reza1232323 <reza88138890@gmail.com>
2026-07-29 17:00:53 +0330

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -10,9 +10,9 @@ async def add_points(update: Update, context: ContextTypes.DEFAULT_TYPE):
         val = int(update.message.text.split()[2])
         target_id = update.message.reply_to_message.from_user.id
         db.update_field(target_id, "points", val)
-        await update.message.reply_text(f"👑 مقدار {val} پوینت به کاربر اضافه شد.")
-    except:
-        await update.message.reply_text("❌ مثال: `افزایش پوینت 100`", parse_mode='Markdown')
+        await update.message.reply_text(f"👑 مقدار **{val}** پوینت به کاربر اضافه شد.", parse_mode='Markdown')
+    except Exception:
+        await update.message.reply_text("❌ فرمت درست: `افزایش پوینت 100` (روی ریپلای)", parse_mode='Markdown')
 
 async def remove_points(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message.reply_to_message:
@@ -22,6 +22,30 @@ async def remove_points(update: Update, context: ContextTypes.DEFAULT_TYPE):
         val = int(update.message.text.split()[2])
         target_id = update.message.reply_to_message.from_user.id
         db.update_field(target_id, "points", -val)
-        await update.message.reply_text(f"👑 مقدار {val} پوینت از کاربر کم شد.")
-    except:
-        await update.message.reply_text("❌ مثال: `کاهش پوینت 100`", parse_mode='Markdown')
+        await update.message.reply_text(f"👑 مقدار **{val}** پوینت از کاربر کم شد.", parse_mode='Markdown')
+    except Exception:
+        await update.message.reply_text("❌ فرمت درست: `کاهش پوینت 100` (روی ریپلای)", parse_mode='Markdown')
+
+async def add_level(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.message.reply_to_message:
+        await update.message.reply_text("❌ روی پیام کاربر ریپلای کنید.")
+        return
+    try:
+        val = int(update.message.text.split()[2])
+        target_id = update.message.reply_to_message.from_user.id
+        db.update_field(target_id, "level", val)
+        await update.message.reply_text(f"👑 **{val}** سطح (لول) به کاربر اضافه شد.", parse_mode='Markdown')
+    except Exception:
+        await update.message.reply_text("❌ فرمت درست: `افزایش لول 1` (روی ریپلای)", parse_mode='Markdown')
+
+async def remove_level(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.message.reply_to_message:
+        await update.message.reply_text("❌ روی پیام کاربر ریپلای کنید.")
+        return
+    try:
+        val = int(update.message.text.split()[2])
+        target_id = update.message.reply_to_message.from_user.id
+        db.update_field(target_id, "level", -val)
+        await update.message.reply_text(f"👑 **{val}** سطح (لول) از کاربر کم شد.", parse_mode='Markdown')
+    except Exception:
+        await update.message.reply_text("❌ فرمت درست: `کاهش لول 1` (روی ریپلای)", parse_mode='Markdown')
