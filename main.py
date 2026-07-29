c4164c97214abb593b2664dabc44d7d72a1ef49d
reza1232323 <reza88138890@gmail.com>
2026-07-29 19:37:16 +0330

    Update main.py

--- main.py
+++ main.py
@@ -35,9 +35,10 @@ async def router(update: Update, context: ContextTypes.DEFAULT_TYPE):
         elif text.startswith("همگانی"):
             await admin.broadcast_message(update, context); return
 
-    # 🎮 دستورات عمومی
+  # 🎮 دستورات عمومی
     if text == "هاپ": await pet.claim_hop(update, context, user)
     elif text in ["هاپوهام", "پروفایل"]: await pet.show_profile(update, context, user)
+    elif text in ["راهنما", "help"]: await pet.show_help(update, context)  # 👈 اضافه شد
     elif text in ["خرید سگ", "ارتقا سگ"]: await pet.buy_dog(update, context, user)
     elif text in ["غذا", "غذا دادن"]: await pet.feed_dog(update, context, user)
     elif text.startswith("بانک"): await economy.bank_status(update, context, user)
