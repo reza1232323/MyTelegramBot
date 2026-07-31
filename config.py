b6feb0d3e18459b0d188cf370b83fbbd97d5aa03
reza1232323 <reza88138890@gmail.com>
2026-07-31 19:30:23 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# توکن ربات (حتماً توکن جدید جایگزین شود)
+BOT_TOKEN = "8914754707:AAGTVYh2hnwWGZPIP3EqjUFBqwX43fn9P5U"
+
+# آیدی عددی ادمین‌های اصلی
+ADMIN_IDS = [6989311624, 6532150691]
+
+# آیدی یا یوزرنیش کانال‌ها (همراه با @)
+REQUIRED_CHANNELS = [
+    {"name": " کانال اول", "username": "@esmok_shop_poy", "url": "https://t.me/esmok_shop_poy"},
+    {"name": " کانال دوم", "username": "@CODMSAOPZX", "url": "https://t.me/CODMSAOPZX"}
+]
