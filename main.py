ba5b07a1bb035335cb91b5ed6ed1372b93526299
reza1232323 <reza88138890@gmail.com>
2026-07-31 17:17:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
 from config import config
 from handlers import router
 from database import init_db
@@ -10,7 +9,6 @@ import os
 
 logging.basicConfig(level=logging.INFO)
 
-# جلوگیری از اجرای همزمان
 if os.name == 'nt':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
@@ -19,9 +17,9 @@ dp = Dispatcher()
 dp.include_router(router)
 
 async def main():
-    # راه‌اندازی دیتابیس
     init_db()
     print("✅ ربات هاپو راه‌اندازی شد!")
+    print("📱 هم در پیوی و هم در گروه کار میکنه!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
